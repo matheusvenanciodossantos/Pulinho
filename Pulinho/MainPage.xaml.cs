3cbf5e63ccf9d14dfc16f9da703fadb48b8ce31b
Matheus <matheusvenanciodc@gmail.com>
2024-11-05 10:55:28 -0300

    arrumar pra ele rodar certo

--- Pulinho/MainPage.xaml.cs
+++ Pulinho/MainPage.xaml.cs
@@ -7,6 +7,7 @@ public partial class MainPage : ContentPage
 
 	bool Isdead = false;
 	// se está morto
+
 	bool Isjumping = false;
 	//se está pulando
 
@@ -24,7 +25,7 @@ public partial class MainPage : ContentPage
 	int SpeedTwo = 0;
 	//velocidade da segunda camada
 
-	int SpeedTree = 0;
+	int SpeedThree = 0;
 	// velocidade da terceira camada
 
 	int PrimalFloorSpeed = 0;
@@ -42,29 +43,96 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-//------------------------------------------------------------------------//
-    protected override void OnSizeAllocated(double w, double h)
-    {
-        base.OnSizeAllocated(w, h);
-		FixScreenSize(w, h);
-		calculateSpeed(w);
-    }
+
+
 	//------------------------------------------------------------------------//
 
-	void FixScreenSize (double w, double h)
+
+
+	//------------------------------------------------------------------------//
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		FixScreenSize(w, h);
+		CalculateSpeed(w);
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Drawn();
+	}
+	//------------------------------------------------------------------------//
+
+	void FixScreenSize(double w, double h)
 	{
 		foreach (var A in HSLayerOne.Children)
-		(A as Image).WidthRequest = w;
+			(A as Image).WidthRequest = w;
 		foreach (var A in HSLayerTwo.Children)
-		(A as Image).WidthRequest = w;
-		foreach (var A in HSLayerTree.Children)
-		(A as Image).WidthRequest = w;
+			(A as Image).WidthRequest = w;
+		foreach (var A in HSLayerThree.Children)
+			(A as Image).WidthRequest = w;
 		foreach (var A in HSLayerPrimalfloor.Children)
-		(A as Image).WidthRequest = w;
-        
-		 HSLayerOne.WidthRequest = w * 1.5;
-		 HSLayerTwo.WidthRequest = w * 1.5;
-		 HSLayerTree.WidthRequest = w * 1.5;
-		 HSLayerPrimalfloor.WidthRequest = w * 1.5;
+			(A as Image).WidthRequest = w;
+
+		HSLayerOne.WidthRequest = w * 1.5;
+		HSLayerTwo.WidthRequest = w * 1.5;
+		HSLayerThree.WidthRequest = w * 1.5;
+		HSLayerPrimalfloor.WidthRequest = w * 1.5;
 	}
+
+	//------------------------------------------------------------------------//
+	void CalculateSpeed(double w)
+	{
+		SpeedOne = (int)(w * 0.001);
+		SpeedTwo = (int)(w * 0.004);
+		SpeedThree = (int)(w * 0.008);
+		PrimalFloorSpeed = (int)(w * 0.01);
+	}
+
+	//------------------------------------------------------------------------//
+	async Task Drawn()
+	{
+		while (!Isdead)
+		{
+			ManageScenes();
+			await Task.Delay(TimeToFrame);
+		}
+	}
+
+	//------------------------------------------------------------------------//
+	void MoveScene()
+	{
+		HSLayerOne.TranslationX -= SpeedOne;
+		HSLayerTwo.TranslationX -= SpeedTwo;
+		HSLayerThree.TranslationX -= SpeedThree;
+		HSLayerPrimalfloor.TranslationX -= PrimalFloorSpeed;
+	}
+
+	//------------------------------------------------------------------------//
+	void ManageScenes()
+	{
+		MoveScene();
+		ManageScene(HSLayerOne);
+		ManageScene(HSLayerTwo);
+		ManageScene(HSLayerThree);
+		ManageScene(HSLayerPrimalfloor);
+	}
+
+
+	//------------------------------------------------------------------------//
+
+	void ManageScene(HorizontalStackLayout HSL)
+	{
+		var view = (HSL.Children.First() as Image);
+		if (view.WidthRequest + HSL.TranslationX < 0)
+		{
+			HSL.Children.Remove(view);
+			HSL.Children.Add(view);
+			HSL.TranslationX = view.TranslationX;
+		}
+	}
+
+
+	//------------------------------------------------------------------------//
 }
